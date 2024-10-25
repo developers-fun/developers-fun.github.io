59a16e1e436ae2d9e5db593f3c9c5f6242cfbf8e
Xpectedlol <JOjooakpark@gmail.com>
2024-10-25 17:19:42 -0500

    1

--- js/PercentageCalculator.js
+++ js/PercentageCalculator.js
@@ -18,5 +18,18 @@ function Convertfraction() {
     results.innerText = `${Answer}%`
 }
 function ConvertPercentage() {
-    
+    let decimal = document.getElementById('Percentage').value
+    let results = document.getElementById('Decrsults')
+
+        if (!decimal.startsWith('0.') && !decimal.startsWith('.')) {
+            decimal = `0.${decimal}`
+        }
+        if (!decimal.startsWith('.')) {
+            return
+        }
+
+        let gcd = (a, b) => b ? gcd(b, a % b) : a;
+        let [numerator, denominator] = [Math.round(decimal * 1e5), 1e5];
+        let divisor = gcd(numerator, denominator);
+        results.innerText = `${numerator / divisor}/${denominator / divisor}`;
 }
